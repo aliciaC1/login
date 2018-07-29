d95ab8eddc6d50d5401153f24a46fe0431592709
Jordan Russell <jordanrussell1@gmail.com>
2018-07-29 13:36:36 -0400

    working login and logout page no css styling

---
+++ login.js
@@ -0,0 +1,41 @@
+//wrapped in a function so everything within function is private
+(function(){
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    //when login is succesful redirects you to the main.html page
+    signInSuccessUrl: 'main.html',
+    
+    //used email sign in but can change it - the others are a bit more involved but could figure it out
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: 'terms-of-service.html',
+    // Privacy policy url.
+    //privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+  
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+})()
